a3767b05ccc5ade7bd71c3424c552dcd2a80b7d7
Suplab Debnath <suplabdude@gmail.com>
2021-07-20 20:00:38 +0530

    Add files via upload

--- movies-api/src/main/java/com/microservices/api/movies/feign/ReviewsFeignClient.java
+++ movies-api/src/main/java/com/microservices/api/movies/feign/ReviewsFeignClient.java
@@ -4,13 +4,13 @@ import com.microservices.api.movies.feign.hystrix.ReviewsApiFallbackFactory;
 import com.microservices.api.movies.model.MovieReview;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @FeignClient(name="reviews-api", fallbackFactory = ReviewsApiFallbackFactory.class)
 public interface ReviewsFeignClient {
 
-    @GetMapping("/reviews")
-    List<MovieReview> getMovieReviews(@RequestParam("movieID") Long movieId);
+    @GetMapping("/reviews/{movieID}")
+    List<MovieReview> getMovieReviews(@PathVariable("movieID") Long movieId);
 }
