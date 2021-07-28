315ab1c683861ce781f4926f15e9b2ac57c61c92
Suplab Debnath <suplabdude@gmail.com>
2021-07-28 11:47:10 +0530

    mysql grant

--- mysql/grant.sql
+++ mysql/grant.sql
@@ -1,4 +1,9 @@
+--create database
+CREATE DATABASE IF NOT EXISTS moviesdb;
+-- grant all privileges to root
 grant all privileges on *.* to 'root'@'localhost';
 flush privileges;
+-- allow all hosts to connect
 update user set host='%' where user='root';
+commit;
 flush privileges;
